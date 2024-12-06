7c9cf22dc1bf9151cd207390e33f65aab2ff49c0
David Garcia <hi@davidgarcia.dev>
2024-12-06 18:21:49 +0000

    chore: add base theme

---
+++ docs/source/conf.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+from datetime import date
+
+# Add the parent directory to the system path
+sys.path.insert(0, os.path.abspath(".."))
+
+# Project information
+project = "Blue Robotics Sphinx Theme"
+copyright = f"{date.today().year}, Blue Robotics. All rights reserved."
+author = "Blue Robotics Project Contributors"
+
+# General configuration
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.todo",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.extlinks",
+    "sphinx_blue_robotics_theme",
+    "myst_parser",
+]
+master_doc = "index"
+source_suffix = [".rst", ".md"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# Syntax highlighting style
+pygments_style = "sphinx"
+
+# Substitutions
+rst_prolog = """
+.. |rst| replace:: restructuredText
+"""
+
+# Myst Parser options
+myst_enable_extensions = ["colon_fence"]
+
+# HTML output configuration
+html_theme = "sphinx_blue_robotics_theme"
+html_theme_path = ["../.."]
+html_theme_options = {
+}
+# end html_theme_options
+
+html_last_updated_fmt = "%d %b %Y"
+htmlhelp_basename = "BlueRoboticsDocumentationdoc"
+html_baseurl = "https://sphinx-theme.bluerobotics.com"
+html_context = {"html_baseurl": html_baseurl}
---
+++ docs/source/examples/admonitions.md
@@ -0,0 +1,2 @@
+# Admonitions
+

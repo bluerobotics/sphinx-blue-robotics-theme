b15d3219a699a42d143280840696ad7ec5ecd66c
David Garcia <hi@davidgarcia.dev>
2025-02-28 15:01:46 +0000

    chore: update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,8 @@ import sys
 from datetime import date
 
 # Global variables
-SITE_URL = "https://bluerobotics.github.io/sphinx-blue-robotics-theme/"
+
+SITE_URL = "https://docs.bluerobotics.com/sphinx-blue-robotics-theme/"
 REPO_URL = "https://github.com/bluerobotics/sphinx-blue-robotics-theme"
 REPO_NAME = "sphinx-blue-robotics-theme"
 PROJECT_NAME ="Blue Robotics Sphinx Theme"
